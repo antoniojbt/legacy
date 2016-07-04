fad5ad33a025dc2ac68cda574f02b7cbab5cabab
AntonioJBT <antonio.berlanga@dpag.ox.ac.uk>
2016-07-04 20:12:25 +0100

    edits to eqtl files

---
+++ BEST-D/eQTL_analysis/bash_00_eQTL_genotype_process.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash 00_eQTL_genotype_process.sh \
+	P140343-Results_FinalReport_clean_SNPs_autosome_individuals \
+	P140343-Results_FinalReport_clean_SNPs_autosome_individuals.matrixQTL \
+	P140343-Results_FinalReport_clean_SNPs_autosome_individuals.A-transpose \
+	P140343-Results_FinalReport_clean_SNPs_autosome_individuals.A-transpose.matrixQTL.geno
+
--- BEST-D/eQTL_analysis/qsub_02_eQTL.sh
+++ BEST-D/eQTL_analysis/qsub_02_eQTL.sh
@@ -18,7 +18,7 @@
 
 #Run the job - program must have full path:
 /ifs/apps/apps/R-3.1.3/bin/Rscript \
-	/ifs/devel/antoniob/projects/BEST-D/02_eQTL_order_and_match_vers3.R \
+	02_eQTL_order_and_match_vers3.R \
 	genotype_data_all_treated_baseline.tsv \
 	GEx_baseline_4000_and_2000.tsv \
 	principal_components_normalised_filtered_PC20.tsv \
