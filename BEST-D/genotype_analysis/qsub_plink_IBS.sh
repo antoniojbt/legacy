025c1401dcad37fa3fe9fa403d7e5064eb84d290
AntonioJBT <a.berlanga@imperial.ac.uk>
2016-09-09 15:26:50 +0100

    updating symlinks to actual files

--- BEST-D/genotype_analysis/qsub_plink_IBS.sh
+++ BEST-D/genotype_analysis/qsub_plink_IBS.sh
@@ -1,1 +1,4 @@
-/ifs/projects/proj043/analysis.dir/genotypes.dir/qsub_plink_IBS.sh
+#!/bin/bash
+
+#Run the job - program must have full path:
+/ifs/apps/bio/plink-1.07/plink-1.07-x86_64/plink --noweb --bfile P140343-Results_FinalReport --extract plink.prune.in --genome --out P140343-Results_FinalReport_pruned
