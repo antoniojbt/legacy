63789a30ac14d59031656f28c31907bff5130548
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-03-23 19:26:40 +0000

    cytokine plot updates

--- BEST-D/bestd_lm_cytokine.R
+++ BEST-D/bestd_lm_cytokine.R
@@ -498,6 +498,43 @@ plots <- arrangeGrob(grobs = all_plots, ncol = length(cytokines_0))
 # plots <- arrangeGrob(grid.arrange(grobs = all_plots, ncol = length(cytokines_0)), legend, nrow = 2)
 ggsave('scatterplots_cytokines_and_VD.png', plots, height = 15, width = 15)
 ##########
+
+##########
+# TO DO: plot for all cytokines? Make transparent, change labels
+# Boxplot and stripchart for pre/post and change in value
+# https://www.r-bloggers.com/visualizing-small-scale-paired-data-combining-boxplots-stripcharts-and-confidence-intervals-in-r/
+
+# Set data:
+pre <- imp_all_data_completed$Ln_IL6_0
+post <- imp_all_data_completed$Ln_IL6_12
+
+# Plot:
+png('boxplot_scatterplot_pre_post.png', width = 7.3, height = 5, units = 'in', res = 600)
+par(mfrow = c(1, 2))
+# First graph
+s <- seq(length(pre))
+par(bty = "l")
+boxplot(pre, post, main = "Raw data", 
+        xlab = "Time", ylab = "Measure", 
+        names=c("pre", "post"), col = c("lightblue", "lightgreen"))
+stripchart(list(pre, post), vertical = T, 
+           pch = 16, method = "jitter", 
+           cex = 0.5, add = T)
+segments(rep(0.95, length(pre))[s], pre[s], rep(2, length(pre))[s],
+         post[s], col = 1, lwd = 0.5)
+# Second graph
+# Confidence intervals:
+res <- t.test(post, pre, paired = T, conf.int = T)
+# res <- wilcox.test(post, pre, paired = T, conf.int = T)
+stripchart(post-pre, vertical = T, pch = 16, 
+           method = "jitter", main = "Difference", 
+           ylab = "Difference: Post – Pre", xlab = "Median +/- 95% CI")
+points(1, res$estimate, col = "red", pch = 16, cex = 2)
+arrows(1, res$conf.int[1], 1, res$conf.int[2], col = "red",
+       code = 3, lwd = 3, angle = 90)
+abline(h = 0, lty = 2) # Zero-effectline
+dev.off()
+##########
 #############################################
 
 
